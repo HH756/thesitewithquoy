bd18d434fd083f2416338e96e52abb19b290888f
HH756 <hh756@tutanota.com>
2022-10-31 19:21:26 -0700

    Added the intial stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='sideSelectElement'>
+        Choose your side.
+      </div>
+      <div className="Quoy">
+        <div className='profileImageQuoy'>
+          <img className="QuoyImage"src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.79wy_MZqPldQZYfyuQjFkAHaFj%26pid%3DApi&f=1&ipt=e1d6a2e7f5a2e3ee8dc951be89c5fbfd43b7eaa14cf3c866a25f12fdb28b3ad2&ipo=images"></img>
+        </div>
+        <div className='enterQuoyTextCont'>
+          Quoy
+        </div>
+      </div>
+      <div className="HH756">
+        <div className='profileImageHH756'>
+
+        </div>
+        <div className='enterHH756'>
+          <div className='enterHH756TextCont'>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
