b74f3421c5ba8396030961226ec9c6a1eb6d6011
hh756 <hh756@tutanota.com>
2022-10-31 19:56:35 -0700

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -7,21 +7,19 @@ function App() {
         Choose your side.
       </div>
       <div className="Quoy">
-        <div className='profileImageQuoy'>
-          <img className="QuoyImage"src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.79wy_MZqPldQZYfyuQjFkAHaFj%26pid%3DApi&f=1&ipt=e1d6a2e7f5a2e3ee8dc951be89c5fbfd43b7eaa14cf3c866a25f12fdb28b3ad2&ipo=images"></img>
+        <div className="profileImageQuoyContainer">
+          <img className="profileImage" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.79wy_MZqPldQZYfyuQjFkAHaFj%26pid%3DApi&f=1&ipt=e1d6a2e7f5a2e3ee8dc951be89c5fbfd43b7eaa14cf3c866a25f12fdb28b3ad2&ipo=images"></img>
         </div>
         <div className='enterQuoyTextCont'>
           Quoy
         </div>
       </div>
       <div className="HH756">
-        <div className='profileImageHH756'>
-
+        <div className="profileImageHH756">
+          <img className="profileImage" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.businessinsider.com%2Fimage%2F5c448fe62bdd7f18ce693af2&f=1&nofb=1&ipt=f67620da191e4d2a950681214f26b74bf9b2c34cefa2476a2395406583cb05cb&ipo=images"></img>
         </div>
-        <div className='enterHH756'>
-          <div className='enterHH756TextCont'>
-
-          </div>
+        <div className="enterHH756TextCont">
+          HH756
         </div>
       </div>
     </div>
